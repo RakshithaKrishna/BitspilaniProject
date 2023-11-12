b9a0aaae18e5dd64403c19c365fddaede02c5b72
rakshitha <Rakshithakrishna1997@gmail.com>
2023-11-12 22:56:21 +0530

    second

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -19,12 +19,12 @@ const Register = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      await axios.post("/auth/register", inputs);
+    // try {
+    //   await axios.post("/auth/register", inputs);
       navigate("/login");
-    } catch (err) {
-      setError(err.response.data);
-    }
+    // } catch (err) {
+    //   setError(err.response.data);
+    // }
   };
 
   return (
